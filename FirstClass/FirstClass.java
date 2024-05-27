fd309e99c982842435fb551668b14cffc133cbe8
HirvaPatel30 <169177682+HirvaPatel30@users.noreply.github.com>
2024-05-27 20:30:24 +0530

    Add files via upload

---
+++ FirstClass/FirstClass.java
@@ -0,0 +1,26 @@
+package hirva;
+
+public class FirstClass {
+
+	public static void main(String[] args) 
+	{
+		
+		int a = 35;
+		int b = 12;
+		int ans;
+			
+		ans=a+b;
+		System.out.println("Addition is: "+a+"+"+b+"="+ans);
+			
+		ans=a-b;
+		System.out.println("Subtraction is: "+a+"-"+b+"="+ans);
+			
+		ans=a*b;
+		System.out.println("Multiplication is: "+a+"*"+b+"="+ans);
+		
+		ans=a/b;
+		System.out.println("Division is: "+a+"/"+b+"="+ans);
+			
+	}
+
+}
